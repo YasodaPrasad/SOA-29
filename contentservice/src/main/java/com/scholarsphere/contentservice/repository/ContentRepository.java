0a8915b3569fde8494041e00077898ae31f3ab3c
SiyyadriDeviramDheeraj <deviramdheeraj1010@gmail.com>
2026-09-21 10:27:29 +0530

    feat: complete accessservice microservice on port 8083

--- contentservice/src/main/java/com/scholarsphere/contentservice/repository/ContentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.scholarsphere.contentservice.repository;
-
-import com.scholarsphere.contentservice.entity.Content;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContentRepository extends JpaRepository<Content, Long> {
-}
